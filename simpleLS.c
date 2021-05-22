ebedb127fd34e19013ce267e6447974e0f2034c6
Michael Day <michael.day@gtri.gatech.edu>
2021-05-22 17:06:31 -0400

    modified to stat files sizes

--- simpleLS.c
+++ simpleLS.c
@@ -4,23 +4,36 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+#include <sys/stat.h>
+
 int main (int argc, char *argv[]) {
     DIR *dp;
     struct dirent *dirp;
+
+    int printSizes = 1;
+    long long fileSize;
+    struct stat buffer;
+    int statRet; 
 
     if (argc != 2) {
         //Linux typically prints usage statements like this
         printf("usage: ls directory_name\n");
         return 1;
     }
-
+    
+    if (printSizes) {
+        statRet = stat("/home/mday39/add-to-landscape.sh", &buffer);
+        fileSize = buffer.st_size;
+    } 
+    
     if ((dp = opendir(argv[1])) == NULL) {
         printf("can't open %s\n", argv[1]);
         return 2;
     }
 
-    while ((dirp = readdir(dp)) != NULL)
+    while ((dirp = readdir(dp)) != NULL) {
         printf("%s\n", dirp->d_name);
+    }
 
     closedir(dp);
 
